233e116c36ddf3d8e7dc0859a0b58d868cbb1224
Joanna Tomassoni <jctomassoni@gmail.com>
2021-09-18 13:57:51 -0500

    Add placeholder images to shop page and change favicon

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -7,9 +8,24 @@ const ShopPage = () => (
   <div>
   <Layout>
     <Seo title="Shop" />
-    <p class='m-4 p-4'>
-        Two different links -- one to first album and one to new album
-    </p>
+    <div class='m-auto p-4 flex align-middle'>
+      <p class='m-auto p-4'>
+        <StaticImage
+          src="https://picsum.photos/300"
+          class="m-8 p-4"
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt="placeholder"
+        />
+      </p>
+      <p class='m-auto p-4'>
+        <StaticImage
+          src="https://picsum.photos/300"
+          class="m-8 p-4"
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt="placeholder"
+        />
+      </p>
+    </div> 
   </Layout>
   </div>
 )
