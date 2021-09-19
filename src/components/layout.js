cc9ecf307cd6538d880d65911059f95dac1454d6
Joanna Tomassoni <jctomassoni@gmail.com>
2021-09-19 10:28:19 -0500

    cleanup, add dotenv and bandsintown source plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,7 +24,7 @@ const Layout = ({ children }) => {
 
   return (
     <div class='max-h-screen'>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header siteTitle={data.site.siteMetadata?.title} />
       <div class='mx-auto my-1 text-center'>
         <main>{children}</main>
         <footer class='mt-8 my-4 text-xs'>
