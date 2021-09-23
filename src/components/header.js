fb91cb42688a047a4b3c32a6b65db931fcdce91d
Joanna Tomassoni <jctomassoni@gmail.com>
2021-09-23 08:57:05 -0500

    Change nav header to flex
    I was debating between using flex and grid for the header. It's still not clear to me what the major use cases are for either. All I know is that the flexbox design I have now looks much better than the grid I had attempted to implement.
    
    Still have to make a lot of this responsive/mobile-friendly.

--- src/components/header.js
+++ src/components/header.js
@@ -2,38 +2,9 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-const grid = {
-  display: 'grid',
-  gridTemplateColumns: '7fr 1fr repeat(5, 0.5fr)',
-  gridTemplateRows: '1fr',
-  gridColumnGap: '0px',
-  gridRowGap: '0px',
-}
-
 const Header = ({ siteTitle }) => (
   <nav class='bg-black'>
-    <div class='my-0 mx-0 ml-8 w-11/12 py-6 align-bottom' style={grid}>
-      <Link exact to="/">
-        <p class='gold-text text-3xl font-title'>{siteTitle}</p>
-      </Link>
-      <div></div>
-      <Link to="/about" className='mt-3 text-right' activeClassName='text-yellow-400'>
-        <p>About</p>
-      </Link>
-      <Link to="/shows" className='mt-3 text-right' activeClassName='text-yellow-400'>
-        <p>Shows</p>
-      </Link>
-      <Link to="/press" className='mt-3 text-right' activeClassName='text-yellow-400'>
-        <p>Press</p>
-      </Link>
-      <Link to="/music" className='mt-3 text-right' activeClassName='text-yellow-400'>
-        <p>Music</p>
-      </Link>
-      <Link to="/shop" className='mt-3 text-right' activeClassName='text-yellow-400'>
-        <p>Shop</p>
-      </Link>
-    </div>
-    {/* <div class='my-0 mx-0 ml-8 w-11/12 py-6 flex justify-between'>
+    <div class='my-0 mx-0 ml-8 w-11/12 py-6 flex justify-between'>
       <Link exact to="/">
         <p class='gold-text text-3xl font-title'>{siteTitle}</p>
       </Link>
@@ -54,7 +25,7 @@ const Header = ({ siteTitle }) => (
           <p>Shop</p>
         </Link>
       </div>
-    </div> */}
+    </div>
   </nav>
 )
 
