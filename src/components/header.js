d39cb1d8f27f508de290faa2fc5de9f99c9cb877
Joanna Tomassoni <jctomassoni@gmail.com>
2021-10-01 13:54:39 -0500

    Absolute positioning for mobile dropdown menu
    Previously, the mobile dropdown menu, when active, would push the main page content down. Now, it's positioning is absolute and, when active, sits on top of the main content instead. Also changed some padding for visual optimization.

--- src/components/header.js
+++ src/components/header.js
@@ -6,17 +6,17 @@ import { Link } from "gatsby"
 const Header = () => {
   const [isExpanded, toggleExpansion] = useState(false);
   return (
-    <nav class="flex items-center justify-between flex-wrap p-6 bg-gray-900">
-      <div class="flex items-center flex-shrink-0 mr-6 pb-3">
+    <nav class="flex items-center justify-between flex-wrap bg-gray-900 pb-5">
+      <div class="flex items-center flex-shrink-0 mr-6 p-5">
         <Link to="/" class="font-semibold text-xl tracking-tight"><p class="gold-text text-3xl font-title">Psychedelic Roses</p></Link>
       </div>
-      <div class="block lg:hidden">
+      <div class="block lg:hidden pl-5">
         <button onClick={() => toggleExpansion(!isExpanded)} class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
           <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
         </button>
       </div>
       <div className={`${ isExpanded ? `block` : `hidden` } w-full block flex-grow lg:flex lg:items-center lg:w-auto`}>
-        <div class="text-sm lg:flex-grow">
+        <div class="absolute text-sm lg:flex-grow bg-gray-900 w-full">
           <Link to="/about" class="block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4" activeClassName="block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4">
             About
           </Link>
