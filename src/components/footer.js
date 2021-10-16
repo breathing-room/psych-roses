44725ec46d4fafac1473c3a64292e8f1125d48b2
Joanna Tomassoni <jctomassoni@gmail.com>
2021-10-16 11:16:13 -0500

    social media links, footer spacing

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,13 @@
 import React from "react"
+import { SocialIcon } from "react-social-icons";
 
 const Footer = () => (
   <footer class='my-5 mx-auto text-center text-xs'>
     <div class='inline-flex space-x-4'>
-      <div>© {new Date().getFullYear()}</div>
-      <div>SP</div>
-      <div>FB</div>
+      <SocialIcon url="https://www.facebook.com/taschedelarocha/" network="facebook" style={{ height: 25, width: 25 }} bgColor="#FDE68A"/>
+      <SocialIcon url="mailto:taschedelarocha@gmail.com" network="email" style={{ height: 25, width: 25 }} bgColor="#FDE68A"/>
     </div>
+    <div class='pt-2'>© {new Date().getFullYear()} Billie Cat Development</div>
   </footer>
 )
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -24,7 +24,7 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <div class='min-h-screen'>
+    <div class='min-h-screen flex flex-col justify-between'>
       <Header siteTitle={data.site.siteMetadata?.title} />
       <div class='mx-auto my-1 text-center'>
         <main>{children}</main>
