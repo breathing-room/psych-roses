0fe6d0ab89dd14e1d24aff08bc69b0b597da2b80
Joanna Tomassoni <jctomassoni@gmail.com>
2021-10-23 20:16:23 -0500

    front page images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,73 @@
 import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage, StaticImage, getImage, withArtDirection } from "gatsby-plugin-image"
 
-const IndexPage = () => (
-  <div>
-    <Layout>
-      <Seo title="Home" />
-      <StaticImage
-        src="../images/oej-gold-dress.jpg"
-        layout="constrained"
-        formats={["AUTO", "WEBP", "AVIF"]}
-        alt="placeholder"
-        class='hidden md:block lg:block w-screen'
+const IndexPage = () => {
+  return (
+    <div>
+      <Layout>
+        <Seo title="Home" />
+        <StaticImage 
+          src="../images/indexImages/oej-gold-dress.jpg"
+          // layout="constrained"
+          // height="1000px"
+          // cropFocos="attention"
+          // fit="cover"
+          class="hidden lg:flex mg:flex"
         />
-      <StaticImage
-        src="../images/oej-gold-dress-cropped.jpg"
-        layout="constrained"
-        formats={["AUTO", "WEBP", "AVIF"]}
-        alt="placeholder"
-        class="md:hidden lg:hidden w-screen"
-      />
-    </Layout>
-  </div>
-)
+        <StaticImage 
+          src="../images/indexImages/oej-gold-dress-cropped.jpg"
+          // layout="constrained"
+          // height="1000px"
+          // cropFocos="attention"
+          // fit="cover"
+          class="lg:hidden mg:hidden"
+        />
+      </Layout>
+    </div>
+  )
+}
 
 export default IndexPage
+
+// const {
+//   allFile: { nodes }
+// } = useStaticQuery(graphql`
+//   # query {
+//   #   allFile(
+//   #     filter: {
+//   #       sourceInstanceName: { eq: "images" }
+//   #       ext: { eq: ".jpg" }
+//   #     }
+//   #     sort: { fields: name, order: DESC }
+//   #   ) {
+//   #     nodes {
+//   #       name
+//   #       sourceInstanceName
+//   #       childImageSharp {
+//   #         gatsbyImageData(
+//   #           layout: FULL_WIDTH
+//   #         )
+//   #       }
+//   #     }
+//   #   }
+//   # }
+//   query {
+//     allFile(filter: {relativeDirectory: {eq: "indexImages"}}) {
+//       nodes {
+//         childrenImageSharp {
+//           gatsbyImageData
+//         }
+//       }
+//     }
+//   }
+// `)
+
+// const images = withArtDirection(getImage(nodes[1]), [
+//   {
+//     media: "(max-width: 700px)",
+//     image: getImage(nodes[0]),
+//   },
+// ])
