792d17d3625053280a37149147b35b7323e70fc8
Joanna Tomassoni <jctomassoni@gmail.com>
2022-01-01 12:15:46 -0600

    remove unndeeded class prop

--- src/components/header.js
+++ src/components/header.js
@@ -28,7 +28,7 @@ const Header = () => {
         absolute inset-y-0 inset-left-0 
         transform transition duration-200 ease-in-out z-50 ${ isExpanded ? `-translate-x-0` : `-translate-x-full`}`}>
         <nav>
-          <div class="">
+          <div>
             <Link to="/" class="hover:bg-red-400">
               <svg class="h-6 m-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
