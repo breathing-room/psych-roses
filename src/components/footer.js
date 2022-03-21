221eff2a6c54d7665419a241fd176a4b9e87a834
joannatomassoni <jctomassoni@gmail.com>
2022-03-21 16:29:19 -0500

    get rid of copyright in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,6 @@ const Footer = () => (
       <SocialIcon url="https://www.facebook.com/taschedelarocha/" network="facebook" style={{ height: 25, width: 25 }} bgColor="#FDE68A"/>
       <SocialIcon url="mailto:taschedelarocha@gmail.com" network="email" style={{ height: 25, width: 25 }} bgColor="#FDE68A"/>
     </div>
-    <div class='pt-2'>© {new Date().getFullYear()} BRD</div>
   </footer>
 )
 
