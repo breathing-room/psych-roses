3d4222195c32426cd726547f8cfb319696966710
joannatomassoni <jctomassoni@gmail.com>
2022-03-22 10:30:16 -0500

    Add logo to top of page

--- src/components/header.js
+++ src/components/header.js
@@ -7,16 +7,16 @@ const Header = () => {
   const [isExpanded, toggleExpansion] = useState(false);
   return (
     <div>
-      {/* menu buttons */}
-      <div class="flex justify-between">
-        {/* icon button */}
-        <Link to="/" class="hover:bg-red-400">
-          <svg class="h-6 m-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+      {/* hamburger menu button */}
+       <button onClick={() => toggleExpansion(!isExpanded) } class="hover:bg-red-400 menu-button fixed top-0 right-0 m-2">
+          <svg xmlns="http://www.w3.org/2000/svg" class='h-6 m-4' fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
-        </Link>
-        {/* title */}
-        <Link to="/"><div class="gold-text font-title text-xl m-3">
+        </button>
+      {/* menu buttons */}
+      <div class="flex justify-center">
+        {/* logo */}
+        <Link to="/"><div class="m-3">
           <StaticImage
             src="../images/tempLogoTransparent.png"
             width={250}
@@ -25,12 +25,6 @@ const Header = () => {
             alt="Logo"
           />
         </div></Link>
-        {/* hamburger menu button */}
-        <button onClick={() => toggleExpansion(!isExpanded) } class="hover:bg-red-400 menu-button">
-          <svg xmlns="http://www.w3.org/2000/svg" class='h-6 m-4' fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button>
       </div>
       {/* sidebar */}
       <div class={`h-full bg-gray-900 w-80
