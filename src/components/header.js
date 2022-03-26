1cd55e0e17b6ff51df1669eb97bb461b26d9f713
joannatomassoni <jctomassoni@gmail.com>
2022-03-25 19:45:59 -0500

    Move header to left and rename Press to EPK

--- src/components/header.js
+++ src/components/header.js
@@ -8,8 +8,8 @@ const Header = () => {
   return (
     <div class="h-full z-50">
       {/* Hamburger menu button */}
-      <div class="flex justify-end">
-       <button onClick={() => toggleExpansion(!isExpanded) } class={`hover:bg-red-400 md:fixed self-end top-0 right-0 m-0` }>
+      <div class="flex">
+       <button onClick={() => toggleExpansion(!isExpanded) } class={`hover:bg-red-400 md:fixed top-0 left-0 m-0` }>
           <svg xmlns="http://www.w3.org/2000/svg" class='h-6 m-4' fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
@@ -17,7 +17,7 @@ const Header = () => {
       </div>
 
       {/* Logo */}
-      <div class="flex justify-center pt-0 md:pt-3 pb-10 md:pb-0">
+      <div class="flex justify-center pt-0 md:pt-3 pb-10 md:mb-10 md:pb-0">
         <Link to="/"><div class="flex align-middle">
           <StaticImage
             src="../images/tempLogoTransparent.png"
@@ -30,12 +30,12 @@ const Header = () => {
       </div>
 
       {/* Sidebar */}
-      <div class={`h-full w-full md:w-80 bg-gray-900
+      <div class={`h-100 w-full md:w-80 bg-gray-900
         absolute inset-y-0 inset-left-0
         transform transition duration-200 ease-in-out z-50 text-center ${ isExpanded ? `-translate-x-0` : `-translate-x-full`}`}>
 
         {/* Close menu button */}
-        <div class="flex justify-center md:justify-end">
+        <div class="flex top-0">
           <button onClick={() => toggleExpansion(!isExpanded) } class="text-xl block cursor-pointer py-3 px-5 hover:bg-red-400"><strong>X</strong></button>
         </div>
 
@@ -56,8 +56,8 @@ const Header = () => {
           <Link to="/photos" class="w-full block py-3 px-4 lg:inline-block lg:mt-0 hover:bg-red-400 transition duration:200" activeClassName="w-full block py-3 px-4 lg:inline-block lg:mt-0 hover:bg-red-400 transition duration:200">
             Photos
           </Link>
-          <Link to="/press" class="w-full block py-3 px-4 lg:inline-block lg:mt-0 hover:bg-red-400 transition duration:200" activeClassName="w-full block py-3 px-4 lg:inline-block lg:mt-0 hover:bg-red-400 transition duration:200">
-            Press
+          <Link to="/epk" class="w-full block py-3 px-4 lg:inline-block lg:mt-0 hover:bg-red-400 transition duration:200" activeClassName="w-full block py-3 px-4 lg:inline-block lg:mt-0 hover:bg-red-400 transition duration:200">
+            EPK
           </Link>
         </nav>
       </div>
---
+++ src/pages/epk.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const PressPage = () => (
+  <Layout>
+    <Seo title="Press" />
+      <div class="mx-auto max-w-2xl leading-normal text-sm">
+        <p class=''>Life is a dying art.</p>
+        <p class=''>Full of complexity, dark musings, whimsy, and rib pokes at redemption, Tasche and The Psychedelic Roses' new album escorts us on a tour of our most secret selves - the voices inside of our heads and the axes we long to grind. It lifts us when we feel heavy, colors us boldly when we wash out to grey, and throws a glorious gauntlet down that ... well, if we can't escape suffering completely, then what's the harm in finding a way to enjoy it just a little?
+    The vocals, rich harmonies and strange siren calls, tap on the glass as we sit inside of ourselves, staring out; wakey wakey. Together with the sometimes raw and sometimes delicate instrumentals, the songs pick up our hearts, carefully shaking and rolling them onto the table as life's eternal game of craps continues.</p>
+        <p class=''>We are in good hands as we move through it all.</p>
+        <p class=''>In The Abyss, there's always a party in progress, with Tasche and The Psychedelic Roses taking the stage.</p>
+      </div>
+  </Layout>
+)
+
+export default PressPage
