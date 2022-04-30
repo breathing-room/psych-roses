6c437134f4ddd8aa46d3e9d0a2b5f7c7409383b1
joannatomassoni <jctomassoni@gmail.com>
2022-04-29 19:45:58 -0500

    Change EPK headers

--- src/pages/epk.js
+++ src/pages/epk.js
@@ -15,7 +15,7 @@ const PressPage = () => (
         <div class="flex flex-col w-9/10 justify-between">
           {/* Bio */}
           <div class="">
-            <p class='m-5 italic text-lg'>Who are the Psychedelic Roses?</p>
+            <p class='m-5 text-lg text-red-400 font-bold'>Who are the Psychedelic Roses?</p>
             <p class='m-5'>Forged by the rhythm of their home base in New Orleans, Tasche and the Psychedelic Roses sculpt sound and ardor, evolving away from the roots of R&B and gospel into a world of rock n roll with twinges of punk and grunge, the rare combination of freshness and timelessness. Rising up in the DIY scene of New Orleans, they’ve attracted a passionate, zealous following through word of mouth for their energetic performance spectacles. </p>
             <p class='m-5'>Their inimitable, soulful vocals with 4-part harmonies and demented musical palette melds elegance and ferocity, coalescing into a singular genre that weaves doo-wop and glitter-gore with a psychedelic surf/punk edge.</p>
             <p class='m-5'>Band leader and producer Tasche de la Rocha began her music career as a teen on the streets of New Orleans, and quickly found recognition for her rare sound, being invited to play solo house concerts for Laurie Anderson and Jennifer Coolidge. In her acclaimed debut album <a href="https://open.spotify.com/album/1c5wv4ZOmfz6mRtcI2pUw4">Gold Rose</a>, Tasche collaborated with Joseph Faison, out of which grew the full 8-piece band Psychedelic Roses. </p>
@@ -28,15 +28,15 @@ const PressPage = () => (
           </div>
           {/* Contact */}
           <div class="">
-            <h4 class='m-5 italic text-lg'>How to get in touch</h4>
+            <h4 class='m-5  text-lg text-red-400 font-bold'>How to get in touch</h4>
             <p class='m-5'>taschedelarocha@gmail.com</p>
           </div>
         </div>
-        <div class="flex flex-col ">
-          <div class="flex">
+        <div class="flex flex-col items-center">
+          <div class="">
             <StaticImage
               src="../images/carouselImages/IMG_2451.jpg"
-              width={900}
+              width={1000}
               class="m-8 z-0"
               formats={["AUTO", "WEBP", "AVIF"]}
               alt="Psychedelic Roses @ Saturn Bar in NOLA"
@@ -45,7 +45,7 @@ const PressPage = () => (
           <div class="flex">
             {/* Reviews */}
             <div class="flex flex-col self-center">
-              <h4 class='italic text-lg'>What folks are saying...</h4>
+              <h4 class=' text-lg text-red-400 font-bold'>What folks are saying...</h4>
               <p class=''>"Life is a dying art.</p>
               <p class=''>Full of complexity, dark musings, whimsy, and rib pokes at redemption, Tasche and The Psychedelic Roses' new album escorts us
                             on a tour of our most secret selves - the voices inside of our heads and the axes we long to grind.
@@ -56,7 +56,7 @@ const PressPage = () => (
                             onto the table as life's eternal game of craps continues.</p>
               <p class=''>We are in good hands as we move through it all.</p>
               <p class=''>In The Abyss, there's always a party in progress, with Tasche and The Psychedelic Roses taking the stage."</p>
-              <p class='italic'>- Tansy Undercrypt</p>
+              <p class=''>- Tansy Undercrypt</p>
             </div>
             {/* Songs */}
             <div class="flex justify-center p-5">
