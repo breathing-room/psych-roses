258f001b0a2f90980ba8d96df13b16de1c9f5157
joannatomassoni <jctomassoni@gmail.com>
2022-05-01 13:12:46 -0500

    EPK formatting fixes

--- src/pages/epk.js
+++ src/pages/epk.js
@@ -11,10 +11,10 @@ const PressPage = () => (
   <Layout>
     <Seo title="Press" />
       {/* Main container */}
-      <div class="flex bg-gray-900 text-justify">
+      <div class="flex-col md:flex-row lg:flex text-justify">
         <div class="flex flex-col w-9/10 justify-between">
           {/* Bio */}
-          <div class="">
+          <div class="bg-gray-900">
             <p class='m-5 text-lg text-red-400 font-bold'>Who are the Psychedelic Roses?</p>
             <p class='m-5'>Forged by the rhythm of their home base in New Orleans, Tasche and the Psychedelic Roses sculpt sound and ardor, evolving away from the roots of R&B and gospel into a world of rock n roll with twinges of punk and grunge, the rare combination of freshness and timelessness. Rising up in the DIY scene of New Orleans, they’ve attracted a passionate, zealous following through word of mouth for their energetic performance spectacles. </p>
             <p class='m-5'>Their inimitable, soulful vocals with 4-part harmonies and demented musical palette melds elegance and ferocity, coalescing into a singular genre that weaves doo-wop and glitter-gore with a psychedelic surf/punk edge.</p>
@@ -23,17 +23,17 @@ const PressPage = () => (
             <p class='m-5'>Psych Roses’ feminist alchemy slays from rags to psychedelic riches, a phoenix from the ashes of garage rock to the temple of maniacal joy.</p>
           </div>
           {/* Video */}
-          <div class="flex  p-5 justify-center">
+          <div class="flex p-5 justify-center">
             <Video videoSrcURL="https://www.youtube.com/embed/ScMzIvxBSi4" videoTitle="Placeholder"/>
           </div>
           {/* Contact */}
-          <div class="">
-            <h4 class='m-5  text-lg text-red-400 font-bold'>How to get in touch</h4>
+          <div class="bg-gray-900 flex-col w-full self-center items-center">
+            <h4 class='m-5 text-lg text-red-400 font-bold'>How to get in touch</h4>
             <p class='m-5'>taschedelarocha@gmail.com</p>
           </div>
         </div>
         <div class="flex flex-col items-center">
-          <div class="">
+          <div class="hidden md:inline">
             <StaticImage
               src="../images/carouselImages/IMG_2451.jpg"
               width={1000}
@@ -42,24 +42,24 @@ const PressPage = () => (
               alt="Psychedelic Roses @ Saturn Bar in NOLA"
             />
           </div>
-          <div class="flex">
+          <div class="m-2 md:flex">
             {/* Reviews */}
-            <div class="flex flex-col self-center">
-              <h4 class=' text-lg text-red-400 font-bold'>What folks are saying...</h4>
-              <p class=''>"Life is a dying art.</p>
-              <p class=''>Full of complexity, dark musings, whimsy, and rib pokes at redemption, Tasche and The Psychedelic Roses' new album escorts us
+            <div class="bg-gray-900 py-2">
+              <p class='m-5 text-lg text-red-400 font-bold'>What folks are saying...</p>
+              <p class='m-5'>"Life is a dying art.</p>
+              <p class='m-5'>Full of complexity, dark musings, whimsy, and rib pokes at redemption, Tasche and The Psychedelic Roses' new album escorts us
                             on a tour of our most secret selves - the voices inside of our heads and the axes we long to grind.
                             It lifts us when we feel heavy, colors us boldly when we wash out to grey, and throws a glorious gauntlet down that ...
                             well, if we can't escape suffering completely, then what's the harm in finding a way to enjoy it just a little?
                             The vocals, rich harmonies and strange siren calls, tap on the glass as we sit inside of ourselves, staring out; wakey wakey.
                             Together with the sometimes raw and sometimes delicate instrumentals, the songs pick up our hearts, carefully shaking and rolling them
                             onto the table as life's eternal game of craps continues.</p>
-              <p class=''>We are in good hands as we move through it all.</p>
-              <p class=''>In The Abyss, there's always a party in progress, with Tasche and The Psychedelic Roses taking the stage."</p>
-              <p class=''>- Tansy Undercrypt</p>
+              <p class='m-5'>We are in good hands as we move through it all.</p>
+              <p class='m-5'>In The Abyss, there's always a party in progress, with Tasche and The Psychedelic Roses taking the stage."</p>
+              <p class='m-5'>- Tansy Undercrypt</p>
             </div>
             {/* Songs */}
-            <div class="flex justify-center p-5">
+            <div class="m-2">
               <SpotifyPlayer
                 uri="spotify:user:Bobby:playlist:0sz2J5ZVROxMv3ZkVMTKuw"
                 size="large"
