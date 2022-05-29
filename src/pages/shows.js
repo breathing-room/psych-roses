63396a8a2fc4259a321bc89f188c508ede450c19
joannatomassoni <jctomassoni@gmail.com>
2022-05-29 12:42:13 -0500

    Add shows json file and use that data for shows, first draft

--- src/pages/shows.js
+++ src/pages/shows.js
@@ -1,41 +1,64 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { useStaticQuery, graphql } from "gatsby"
-import { additionalLineup, allCaps } from '../utils/utils'
+// import { useStaticQuery, graphql } from "gatsby"
+// import { additionalLineup, allCaps } from '../utils/utils'
+const shows = require('../data/showsData.json');
 
 const ShowsPage = () => {
-  const data = useStaticQuery(graphql`
-    query MyQuery {
-      allShow {
-        nodes {
-          artist {
-            id
-            name
-            url
-          }
-          datetime
-          id
-          title
-          url
-          venue {
-            name
-            city
-            region
-          }
-          lineup
-        }
-      }
-    }
-  `)
-
-  const shows = data.allShow.nodes
+  // const data = useStaticQuery(graphql`
+  //   query MyQuery {
+  //     allShow {
+  //       nodes {
+  //         artist {
+  //           id
+  //           name
+  //           url
+  //         }
+  //         datetime
+  //         id
+  //         title
+  //         url
+  //         venue {
+  //           name
+  //           city
+  //           region
+  //         }
+  //         lineup
+  //       }
+  //     }
+  //   }
+  // `)
+  const today = new Date()
 
   return (
     <Layout>
       <Seo title="Shows" />
       <div class='lg:p-6'>
-          {
+        {
+          shows.length ? shows.map((show) => {
+            const date = new Date(show.date)
+            console.log(new Date(show.date))
+            if (date >= today) {
+              return (
+                <div class="w-9/10 p-2">
+                  <div class="md:inline md:p-2">
+                    <i>{show.date}</i>
+                  </div>
+                  <div class="md:inline md:p-2 text-red-400">
+                    {show.city}
+                  </div>
+                  <div class="md:inline md:p-2 text-red-400">
+                    <strong>{show.description}</strong>
+                  </div>
+                </div>
+              )
+            }
+          })
+          :
+          "No shows"
+        }
+          {/* {
             shows.length ? shows.map((show) => {
               let otherArtists = additionalLineup(show);
               let options = { weekday: 'short', month: 'short', day: 'numeric' };
@@ -55,7 +78,7 @@ const ShowsPage = () => {
               )
             })
             : <p class='text-md p-4'>No upcoming shows right now! Stay tuned...</p>
-          }
+          } */}
       </div>
     </Layout>
   )
