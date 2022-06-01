f217a8ae28f305066cce34c0553a46c60f2635fd
joannatomassoni <jctomassoni@gmail.com>
2022-05-31 23:27:01 -0400

    Delete unused code

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,10 +16,6 @@ const IndexPage = ({data}) => {
           <GatsbyImage image={getImage(data.file.childImageSharp.gatsbyImageData)} alt="Album cover"/>
         </div>
         <div class="flex mx-auto md:flex-row w-40">
-        {/* <div class="flex-col w-40 items-center md:flex md:place-content-center md:space-x-4 md:mb-6"> */}
-          {/* <p class="p-2 border-2 mb-4 hover:bg-red-400 font-semibold hover:cursor-pointer">STREAM MUSIC</p> */}
-          {/* <p class="p-2 border-2 mb-4 hover:bg-red-400 font-semibold hover:cursor-pointer">BUY STUFF</p> */}
-        {/* </div> */}
         </div>
     </Layout>
   )
