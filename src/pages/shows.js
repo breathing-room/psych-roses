450ab19fb1e005deefcc6a99b626bd19996141d1
joannatomassoni <jctomassoni@gmail.com>
2022-06-30 20:37:47 -0400

    Add Quintron halloween show

--- src/pages/shows.js
+++ src/pages/shows.js
@@ -6,78 +6,38 @@ import Seo from '../components/seo';
 const shows = require('../data/showsData.json');
 
 function ShowsPage() {
-  // const data = useStaticQuery(graphql`
-  //   query MyQuery {
-  //     allShow {
-  //       nodes {
-  //         artist {
-  //           id
-  //           name
-  //           url
-  //         }
-  //         datetime
-  //         id
-  //         title
-  //         url
-  //         venue {
-  //           name
-  //           city
-  //           region
-  //         }
-  //         lineup
-  //       }
-  //     }
-  //   }
-  // `)
   const today = new Date();
+  const upcomingShows = shows.filter((show) => {
+    const date = new Date(show.date);
+    return date >= today;
+  });
 
   return (
     <Layout>
       <Seo title="Shows" />
-      <div className="lg:p-6">
+      <div class="lg:p-6">
+        <p class="text-4xl font-bold text-red-400 mb-9">UPCOMING SHOWS</p>
         {
-          shows.length ? shows.map((show) => {
+          upcomingShows.length ? upcomingShows.map((show) => {
             const date = new Date(show.date);
-            console.log(new Date(show.date));
             if (date >= today) {
               return (
-                <div className="w-9/10 p-2">
-                  <div className="md:inline md:p-2">
+                <div class="w-9/10 p-2">
+                  <div class="md:inline md:p-2">
                     <i>{show.date}</i>
                   </div>
-                  <div className="md:inline md:p-2 text-red-400">
+                  <div class="md:inline md:p-2 text-red-400">
                     {show.city}
                   </div>
-                  <div className="md:inline md:p-2">
+                  <div class="md:inline md:p-2">
                     <strong>{show.description}</strong>
                   </div>
                 </div>
               );
             }
           })
-            : 'No shows'
+            : <p class="m-4">No shows scheduled... stay tuned!</p>
         }
-        {/* {
-            shows.length ? shows.map((show) => {
-              let otherArtists = additionalLineup(show);
-              let options = { weekday: 'short', month: 'short', day: 'numeric' };
-              return (
-                <div class='lg:grid lg:grid-flow-col lg:ml-48 lg:mr-48 pt-5'>
-                  <div class='font-bold text-yellow-300 my-2 lg:my-5'>{allCaps(new Date(show.datetime).toLocaleString("en-US", options))}</div>
-                  <div class='lg:my-5'>{show.venue.name}</div>
-                  <div class='pl-16 pr-16'></div>
-                  <div class='lg:my-5'>{show.venue.city}, {show.venue.region}</div>
-                  <button class='bg-yellow-500 hover:bg-yellow-700 font-bold py-2 px-2 rounded w-28 my-3 mx-auto'><a href={show.url}>RSVP</a></button>
-                  { otherArtists.length ?
-                    <div>{otherArtists}</div>
-                    :
-                    null
-                  }
-                </div>
-              )
-            })
-            : <p class='text-md p-4'>No upcoming shows right now! Stay tuned...</p>
-          } */}
       </div>
     </Layout>
   );
