d2ab8f109aed1512c23e6101d158337c4ac61c26
joannatomassoni <jctomassoni@gmail.com>
2022-07-01 05:50:07 -0400

    wip music embed fix

--- src/pages/music.js
+++ src/pages/music.js
@@ -7,18 +7,19 @@ function MusicPage() {
   return (
     <Layout>
       <Seo title="Releases" />
-      <div className="m-auto p-4 flex flex-col lg:flex-row justify-center">
-        <div className="p-4">
+      <div className="md:m-auto md:p-4 flex flex-col lg:flex-row justify-center">
+        <div className="md:p-4">
           <MusicPlayer
-            embedUrl="https://bandcamp.com/EmbeddedPlayer/album=960835734/size=large/bgcol=333333/linkcol=e99708/transparent=true/"
+            // embedUrl="https://bandcamp.com/EmbeddedPlayer/album=960835734/size=medium/bgcol=333333/linkcol=e99708/transparent=true/"
+            embedUrl="https://bandcamp.com/EmbeddedPlayer/album=960835734/size=large/bgcol=333333/linkcol=e99708/tracklist=false/transparent=true/"
             bandcampUrl="https://tascheandthepsychedelicroses.bandcamp.com/album/gold-rose"
             title="Gold Rose by Tasche And The Psychedelic Roses"
             class="m-auto"
           />
         </div>
-        <div className="p-4">
+        <div className="md:p-4">
           <MusicPlayer
-            embedUrl="https://bandcamp.com/EmbeddedPlayer/album=2684022796/size=large/bgcol=333333/linkcol=e99708/transparent=true/"
+            embedUrl="https://bandcamp.com/EmbeddedPlayer/album=2684022796/size=large/bgcol=333333/linkcol=e99708/tracklist=false/transparent=true/"
             bandcampUrl="https://tascheandtheangels.bandcamp.com/album/tasche-the-angels-ep"
             title="Tasche &amp; The Angels EP by Tasche &amp; The Angels"
           />
