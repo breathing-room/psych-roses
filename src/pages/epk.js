a951a39a7b0b50189ef9f6c15e3a26e91713d3dd
joannatomassoni <jctomassoni@gmail.com>
2022-12-13 15:35:14 -0500

    Fix EPK

--- src/pages/epk.js
+++ src/pages/epk.js
@@ -26,9 +26,9 @@ function PressPage() {
             />
           </div>
           {/* Bio */}
-          <div className="bg-gray-800 opacity-75">
-            <p className="m-5 text-lg text-red-300 font-bold">Bio</p>
-            <p className="m-5">
+          <div className="bg-gray-800 opacity-75 py-5">
+            <p className="mx-5 text-lg text-red-300 font-bold">Bio</p>
+            <p className="mx-5">
               Forged in the rhythm, joy, and despair of New Orleans,
               <strong> Tasche and the Psychedelic Roses</strong>
               {' '}
@@ -36,8 +36,8 @@ function PressPage() {
               and elements of punk, doo wop, surf, cabaret, and psychedelia,
               all the while swaying on a thin wire between sweetness and destruction.
             </p>
-            <p className="m-5">Throughout the 10 tracks on their self-titled album, band leader and producer Tasche de la Rocha guides the Psychedelic Roses as they build, burn, and rise again. She is equally at home coming together in close harmony with the Angels (as her three backup singers are known) as she is shredding over waves of distortion as one of the three guitarists in the group.</p>
-            <p className="m-5">
+            <p className="mx-5">Throughout the 10 tracks on their self-titled album, band leader and producer Tasche de la Rocha guides the Psychedelic Roses as they build, burn, and rise again. She is equally at home coming together in close harmony with the Angels (as her three backup singers are known) as she is shredding over waves of distortion as one of the three guitarists in the group.</p>
+            <p className="mx-5">
               Tasche began her music career as a teen busking solo on the streets of New Orleans,
               and quickly found recognition for her singular voice and guitar sound,
               playing solo house concerts for Laurie Anderson and Jennifer Coolidge.
@@ -47,7 +47,7 @@ function PressPage() {
               , Tasche collaborated with producer/engineer/guitarist Joseph Faison,
               out of which grew the full eight-piece band the Psychedelic Roses.
             </p>
-            <p className="m-5">
+            <p className="mx-5">
               Tasche and the Psychedelic Roses&apos; self-titled album will be released in fall of
               2022 on New Orleans label Sinking City Records. It was recorded at the Studio In The
               Country in Bogalusa, LA, and Wixmix in New Orleans, and was produced by
@@ -55,7 +55,7 @@ function PressPage() {
               is known for his work with Arcade Fire, Dr. John, 79rs Gang, Soul Rebels,
               and Lost Bayou Ramblers, among others.
             </p>
-            <p className="m-5">
+            <p className="mx-5">
               Tasche and the Psychedelic Roses rock with a wild abandon and channel a
               feminist alchemy. It&apos;s a soundtrack for the moment - make out sessions,
               quiet contemplation, and fighting the demons around us.
@@ -66,10 +66,10 @@ function PressPage() {
             <Video videoSrcURL="https://www.youtube.com/embed/ScMzIvxBSi4" videoTitle="Placeholder" />
           </div> */}
           {/* Contact */}
-          <div className="bg-gray-800 opacity-75 my-5 flex-col w-full self-center items-center">
-            <h4 className="m-5 text-lg text-red-300 font-bold">Get in touch</h4>
-            <p className="mx-5 hover:text-red-300"><a href="mailto:taschedelarocahmusic@gmail.com">taschedelarochamusic@gmail.com</a></p>
-            <div>
+          <div className="bg-gray-800 opacity-75 my-5 py-5 flex-col w-full self-center items-center">
+            <h4 className="mx-5 text-lg text-red-300 font-bold">Get in touch</h4>
+            <p className="mx-5 hover:text-red-300"><a class="link-override" href="mailto:taschedelarocahmusic@gmail.com">taschedelarochamusic@gmail.com</a></p>
+            <div className="mx-7">
               <SocialIcon bgColor="#FEF3C7" fgColor="#F87171" style={{ margin: '1rem', width: '35px', height: '35px' }} url="https://open.spotify.com/artist/0Im7zcYdMp1snHs6JmrB9M?si=RnqkTi0gSJuGTaSAosUdJQ" />
               <SocialIcon bgColor="#FEF3C7" fgColor="#F87171" style={{ margin: '1rem', width: '35px', height: '35px' }} url="https://www.facebook.com/taschedelarocha" />
               <SocialIcon bgColor="#FEF3C7" fgColor="#F87171" style={{ margin: '1rem', width: '35px', height: '35px' }} url="https://www.instagram.com/tascheandthepsychedelicroses/" />
@@ -80,10 +80,10 @@ function PressPage() {
         <div className="flex flex-col items-center my-6">
           <div className="m-2 md:flex">
             {/* Reviews */}
-            <div className="bg-gray-800 opacity-75 py-2">
-              <p className="m-5 text-lg text-red-300 font-bold">What folks are saying...</p>
-              <p className="m-5">&quot;Life is a dying art.</p>
-              <p className="m-5">
+            <div className="bg-gray-800 opacity-75 py-5">
+              <p className="mx-5 text-lg text-red-300 font-bold">What folks are saying...</p>
+              <p className="mx-5">&quot;Life is a dying art.</p>
+              <p className="mx-5">
                 Full of complexity, dark musings, whimsy, and rib pokes at redemption, Tasche
                 and the Psychedelic Roses&apos; new album escorts us on a tour of our most
                 secret selves - the voices inside of our heads and the axes we long to grind.
@@ -96,12 +96,12 @@ function PressPage() {
                 the songs pick up our hearts, carefully shaking and rolling them
                 onto the table as life&apos;s eternal game of craps continues.
               </p>
-              <p className="m-5">We are in good hands as we move through it all.</p>
-              <p className="m-5">
+              <p className="mx-5">We are in good hands as we move through it all.</p>
+              <p className="mx-5">
                 In The Abyss, there&apos;s always a party in progress, with
                 Tasche and the Psychedelic Roses taking the stage.&quot;
               </p>
-              <p className="m-5">- Tansy Undercrypt</p>
+              <p className="mx-5">- Tansy Undercrypt</p>
             </div>
             {/* Songs */}
             <div className="m-2">
