fccc46f5932d66ec639f817ea18e8c32ef7c4bd8
joannatomassoni <jctomassoni@gmail.com>
2023-04-14 16:40:55 -0400

    Fixup residency listing and remove link from shows with no ticket link

--- src/pages/shows.js
+++ src/pages/shows.js
@@ -3,6 +3,13 @@ import Layout from '../components/layout';
 import Seo from '../components/seo';
 const shows = require('../data/showsData.json');
 
+const residency = {
+  "date": "July 20, 2023 - August 20, 2023",
+  "city": "Verplanck, NY",
+  "description": "Residency at KinoSaito -- performances TBA",
+  "tickets_url": ""
+}
+
 function ShowsPage() {
   const today = new Date();
   const upcomingShows = shows.filter((show) => {
@@ -10,6 +17,9 @@ function ShowsPage() {
     return date >= today;
   });
 
+  upcomingShows.push(residency);
+
+  console.log(upcomingShows)
   return (
     <Layout>
       <Seo title="Tasche and the Psychedelic Roses" pathname="/shows" />
@@ -18,7 +28,7 @@ function ShowsPage() {
         {
           upcomingShows.length ? upcomingShows.map((show) => {
             const date = new Date(show.date);
-            if (date >= today) {
+            if (!(date <= today)) {
               return (
                 <div class="w-9/10 p-2">
                   <div class="md:inline md:p-2">
@@ -28,15 +38,13 @@ function ShowsPage() {
                     {show.city}
                   </div>
                   <div class="md:inline md:p-2 hover:text-red-400">
-                    <a class="bootstrap-link-override" href={show.tickets_url} rel="noreferrer noopener" target="_blank"><strong>{show.description}</strong></a>
-                  </div>
-                  {/* <div class="md:inline md:p-2 text-red-300 hover:text-red-400">
                     {
                       show.tickets_url ?
-                       <a class="bootstrap-link-override" href={show.tickets_url} rel="noreferrer noopener" target="_blank"><strong>MORE INFO</strong></a>
-                       : null
+                        <a class="bootstrap-link-override" href={show.tickets_url} rel="noreferrer noopener" target="_blank"><strong>{show.description}</strong></a>
+                        :
+                        <strong>{show.description}</strong>
                     }
-                  </div> */}
+                  </div>
                 </div>
               );
             }
