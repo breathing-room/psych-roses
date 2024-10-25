b9c8ce682a6f612c044e988a3960a00c6ed5fb79
joannatomassoni <jctomassoni@gmail.com>
2024-10-25 11:46:16 -0400

    Add upcoming shows to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@ import { SocialIcon } from 'react-social-icons';
 import "@fontsource/fjalla-one"
 import SpotifyPlayer from '../components/spotifyPlayer';
 import AlbumCoverImg from '../images/album-cover.jpg'
+import HalloweenShowFlyer from '../images/halloween-show-24.jpg'
+import MOHFlyer from '../images/MOH-24-flyer.jpg'
 import Video from '../components/video';
 
 function IndexPage({ data }) {
@@ -14,29 +16,33 @@ function IndexPage({ data }) {
     <Layout>
       <Seo title="Tasche and the Psychedelic Roses" pathname="/" />
       <div className="md:w-9/12 mx-auto header">
-        <div className="md:flex p-5 mb-10">
-          <div className="md:flex md:items-center mx-auto">
-            <div>
-              <p className="text-xl md:mb-5 md:mt-5 lg:my-2 lg:text-3xl font-semibold text-red-400">
-                "HAPPY SONG" OFFICIAL MUSIC VIDEO
-              </p>
-              <p className="lg:hidden text-xl md:mb-5 md:mt-5 lg:my-2 lg:text-3xl font-semibold text-red-300">
-                OUT NOW
-              </p>
-
-              <button className="lg:hidden md:mb-10 text-lg font-semibold bg-red-400 hover:bg-red-300 py-2 pl-5 pr-7 my-3 rounded-full shadow">
-                <a href="https://youtu.be/USTBDhGY8Ig" rel="noopener noreferrer" target="_blank" class="bootstrap-link-override">
-                  <span class="ml-2">WATCH</span>
-                </a>
-              </button>
-
-              <div class="hidden lg:block md:max-w-2xl md:mx-auto ">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/USTBDhGY8Ig" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-              </div>
+        <p className="text-2xl md:mb-5 md:mt-5 lg:my-2 text-lm md:text-4xl font-semibold text-red-300 p-3">
+          UPCOMING SHOWS
+        </p>
+        <div className="md:flex pb-10">
+          <div class="md:max-w-lg md:mx-auto p-4 ">
+            <img class="border-2 border-gray-500"
+              src={MOHFlyer}
+              className="m-auto z-0"
+            />
+            <button className="md:mb-10 my-4 text-md md:text-4xl font-semibold bg-red-400 hover:bg-red-300 py-2 px-4 mx-3 rounded-full shadow">
+              <a href="https://www.eventbrite.com/e/tasche-the-angels-tickets-1031123637557?aff=ebdsoporgprofile" rel="noopener noreferrer" target="_blank" class="bootstrap-link-override">
+                  <span class="ml-2">TICKETS</span>
+              </a>
+            </button>
           </div>
+          <div class="md:max-w-lg md:mx-auto p-4">
+            <img class="border-2 border-gray-500"
+              src={HalloweenShowFlyer}
+              className="m-auto z-0"
+            />
+             <button className="md:mb-10 my-4 text-md md:text-4xl font-semibold bg-red-400 hover:bg-red-300 py-2 px-4 mx-3 rounded-full shadow">
+                <a href="https://tascheandthepsychedelicroses.ticketspice.com/tasche-and-friends" rel="noopener noreferrer" target="_blank" class="bootstrap-link-override">
+                  <span class="ml-2">TICKETS</span>
+                </a>
+            </button>
           </div>
         </div>
-
         <div className="md:flex">
           <div class="md:max-w-2xl md:mx-auto">
             <img
@@ -47,10 +53,6 @@ function IndexPage({ data }) {
 
           <div className="md:flex md:items-center mx-auto">
             <div>
-              <p className="text-2xl md:mb-5 md:mt-5 lg:my-2 text-lm md:text-4xl font-semibold text-red-300">
-                NEW ALBUM OUT NOW
-              </p>
-
               <button className="md:mb-10 text-lg md:text-4xl font-semibold bg-red-400 hover:bg-red-300 py-2 px-4 mx-3 rounded-full shadow">
                 <a href="https://open.spotify.com/album/5WPIAEuO2jz3nBxcowCtwu?si=2EJG7T1aTMiBFOWyBrQEOQ" rel="noopener noreferrer" target="_blank" class="bootstrap-link-override">
                   <SocialIcon bgColor="#FEF3C7" fgColor="#F87171" network="spotify"/>
